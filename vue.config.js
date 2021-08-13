28195845525cf8e433da8de9d77a4b769af390e0
cralixraev <cralixraev@gmail.com>
2021-08-13 14:52:19 +0300

    add config

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: 'LEARNWIKI',
+    themeColor: '#7ED321',
+    msTileColor: '#7ED321',
+    manifestOptions: {
+      background_color: '#417505'
+    }
+  }
+}
