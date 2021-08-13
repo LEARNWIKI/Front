27702d88ed0031fd7fc01eee366111b2cd0fdd81
cralixraev <cralixraev@gmail.com>
2021-08-13 21:28:25 +0300

    Update: Добавили тестовые данные и попытались пообщаться с API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,52 @@
 <template>
   <div class="home">
+    <Diagram
+      :width="2000"
+      :height="1000"
+      :fluid="false"
+      scale="1"
+      background="#fafafa"
+      :nodes="nodes"
+      :links="links"
+      :editable="false"
+      :labels="{
+        edit: 'Edit',
+        remove: 'Remove',
+        link: 'Link',
+        select: 'Select',
+        cancel: 'Cancel',
+      }"
+      @editNode="editNode"
+      @editLink="editLink"
+      @nodeChanged="nodeChanged"
+      @linkChanged="linkChanged"
+    ></Diagram>
   </div>
 </template>
 
 <script>
+import Diagram from '../components/Diagram.vue'
+import data from '@/assets/testdata.json'
+const axios = require('axios')
+
 // @ is an alias to /src
 export default {
   name: 'Home',
   components: {
+    Diagram
+  },
+  mounted () {
+    axios
+      .get('https://teadove.xyz:9999/api/nodes/')
+      .then((response) => {
+        this.nodes = response.data
+      })
+      .catch((error) => {
+        console.log('Ошибка! Нет доступа к API. ' + error)
+      })
+    this.nodes = data.nodes
+    this.links = data.links
+    console.log(this.links)
   }
 }
 </script>
