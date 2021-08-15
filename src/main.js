fdd4b4de5368cdeb4028219b18e51bc2a26d1149
EGPETONT <mitrievich2.gmail.com>
2021-08-15 17:17:28 +0300

    Добавил свои вомменты

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import router from './router'
+import router from './router' // проще работа с ссылками и вьюшками - для синг пейдж приложения
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app') // инициализация приложения - метод Вьюшки
